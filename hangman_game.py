1c938d39b32af07c8823134ceeb4bbd4804ed5f2
RichSawce <132316567+RichSawce@users.noreply.github.com>
2025-12-21 11:26:39 -0500

    Enhance input validation and game logic in Hangman
    Added input validation for letter guesses and improved game flow.

--- hangman_game.py
+++ hangman_game.py
@@ -1,6 +1,7 @@
+import sys
+
 # store word as string
 word = "doughnut"
-
 # replace characters in word with "_"
 codedWord = len(word) * "_"
 # keep list of guessed letters
@@ -10,17 +11,32 @@ maxGuesses = 6
 print("Welcome to Hangman")
 while True:
  print("Word: " + codedWord + "\nMisses: " + str(guessedLetters) + "\nGuesses left: " + str(maxGuesses))
- letter = input("Guess a letter ")
+ letter = input("Guess a letter ").lower()
+# validate input to ensure only one letter is entered at a time
+ if len(letter) > 1 and letter.isalpha():
+      guessedLetters == guessedLetters
+      maxGuesses == maxGuesses
+      print("Enter one letter at a time")
+   
 # Track wrong guesses by checking if guess not in secret_word
- if letter not in word:
-    guessedLetters += [letter]
-    maxGuesses -= 1
+ elif letter in guessedLetters:
+       guessedLetters == guessedLetters
+       maxGuesses == maxGuesses
+       print("You already guessed that letter, try again.")
+  
+ elif letter not in word:
+        guessedLetters += [letter]
+        maxGuesses -= 1
+    
 # end the game when too many wrong guesses or the word is completed.
-    if maxGuesses == 0:
+ if maxGuesses == 0:
       print("You lose! The word was " + word)
-      exit()
+      sys.exit()
 # rebuild the "Word:_a___" display each turn by looping through the secret word
  if letter in word:
       for i in range(len(word)):
          if word[i] == letter:
             codedWord = codedWord[:i] + letter + codedWord[i+1:]
+            if "_" not in codedWord:
+               print("You Win!" + "\nYou correctly guessed the word: " + word)
+               sys.exit()
