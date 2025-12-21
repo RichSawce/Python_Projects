6ad749e3a16a102785a2d1a0c4f35f80bea0cd28
RichSawce <132316567+RichSawce@users.noreply.github.com>
2025-12-21 14:16:29 -0500

    Refactor hangman game to use random word selection
    Enabled random word selection and added input constraints; no more special characters, numbers and only one letter at a time.

--- hangman_game.py
+++ hangman_game.py
@@ -1,7 +1,9 @@
 import sys
+import random
 
 # store word as string
-word = "doughnut"
+list = ["doughnut", "python", "hangman", "challenge", "programming", "developer", "function", "variable"]
+word = random.choice(list)
 # replace characters in word with "_"
 codedWord = len(word) * "_"
 # keep list of guessed letters
@@ -13,15 +15,11 @@ while True:
  print("Word: " + codedWord + "\nMisses: " + str(guessedLetters) + "\nGuesses left: " + str(maxGuesses))
  letter = input("Guess a letter ").lower()
 # validate input to ensure only one letter is entered at a time
- if len(letter) > 1 and letter.isalpha():
-      guessedLetters == guessedLetters
-      maxGuesses == maxGuesses
+ if len(letter) > 1 or not letter.isalpha():
       print("Enter one letter at a time")
    
 # Track wrong guesses by checking if guess not in secret_word
  elif letter in guessedLetters:
-       guessedLetters == guessedLetters
-       maxGuesses == maxGuesses
        print("You already guessed that letter, try again.")
   
  elif letter not in word:
@@ -40,3 +38,4 @@ while True:
             if "_" not in codedWord:
                print("You Win!" + "\nYou correctly guessed the word: " + word)
                sys.exit()
+
