17389dc19f7e8c0a1b6d2108de45b31952f9ae2b
RichSawce <132316567+RichSawce@users.noreply.github.com>
2025-12-20 21:35:34 -0500

    Enhance hangman game with max guesses feature
    Refactor hangman game logic to include max guesses and display remaining guesses.

--- hangman_game.py
+++ hangman_game.py
@@ -1,22 +1,26 @@
 # store word as string
 word = "doughnut"
+
 # replace characters in word with "_"
 codedWord = len(word) * "_"
 # keep list of guessed letters
 guessedLetters = []
 # guesses left
-
+maxGuesses = 6
 print("Welcome to Hangman")
 while True:
- print("Word: " + codedWord + "\nMisses: " + str(guessedLetters))
+ print("Word: " + codedWord + "\nMisses: " + str(guessedLetters) + "\nGuesses left: " + str(maxGuesses))
  letter = input("Guess a letter ")
-# rebuild the "Word:_a___" display each turn by looping through the secret word
+# Track wrong guesses by checking if guess not in secret_word
  if letter not in word:
     guessedLetters += [letter]
- if letter in word:
-    codedWord = codedWord.replace("_", letter)
-   
-
-# Track wrong guesses by checking if guess not in secret_word
-
+    maxGuesses -= 1
 # end the game when too many wrong guesses or the word is completed.
+    if maxGuesses == 0:
+      print("You lose! The word was " + word)
+      exit()
+# rebuild the "Word:_a___" display each turn by looping through the secret word
+ if letter in word:
+      for i in range(len(word)):
+         if word[i] == letter:
+            codedWord = codedWord[:i] + letter + codedWord[i+1:]
