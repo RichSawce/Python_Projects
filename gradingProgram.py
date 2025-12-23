a0f1e1a99fba3bafb30d14d8c7131bb6e032b0c9
RichSawce <132316567+RichSawce@users.noreply.github.com>
2025-12-22 19:03:47 -0500

    Enhance grading program with multiple entries
    Refactored grading program to allow multiple grade entries and added more detailed grade classifications.

--- gradingProgram.py
+++ gradingProgram.py
@@ -1,15 +1,55 @@
-print("enter your grade: ")
-try:
-    score = float(input())
-    if score < 0 or score> 100:
-            print("Please enter a number between 0 and 100")
-    elif score <= 50:
-            print("Failed test")
-    elif score <= 70:
-            print("You got a C")
-    elif score <= 85:
-            print("You got a B")
-    elif score > 85:
-        print("Congratulations, you got an A!")
-except ValueError:
-        print("Invalid input, please enter a number between 0 and 100")
+grades = []
+
+print("Welcome to the Grade Calculator!")
+while True:
+ print("Would you like to enter a grade? (yes/no) ")
+ answer = input()
+
+ if answer.lower() == "yes":
+   print("enter your grade: ")
+   try:
+        score = float(input())
+        grades.append(score)
+        if score < 0 or score> 100:
+                print("Please enter a number between 0 and 100")
+        elif score <= 59:
+                print("Failed test")
+        elif score <= 62:
+                print("You got a D-")
+        elif score <= 66:
+                print("You got a D")
+        elif score <= 69:
+                print("You got a D+")
+        elif score <= 72:
+                print("You got a C-")
+        elif score <= 76:
+                print("You got a C")
+        elif score <= 79:
+                print("You got a C+")
+        elif score <= 82:
+                print("You got a B-")
+        elif score <= 86:
+                print("You got a B")
+        elif score <= 89:
+                print("You got a B+")
+        elif score <= 92:
+                print("You got a A-")
+        elif score <= 96:
+                print("You got a A")
+        else:
+                print("You got a A+")
+   except ValueError:
+            print("Invalid input, please enter a number between 0 and 100")
+            continue
+
+ if answer.lower() == "no":
+        print("Would you like to see all your grades? (yes/no) ")
+        response = input()
+        if response.lower() == "yes":
+            print("Your grades are: " + str(grades))
+            break
+
+        else:
+                response.lower() == "no"
+                print("Goodbye!")
+                break
